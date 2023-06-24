fa5b3944891cf72c9b8a4a1ed831e9c49e730691
amuhtarov <amuhtarov@akumoproject.com>
2023-06-24 14:29:35 -0500

    testing

--- AwsPython.py
+++ AwsPython.py
@@ -3,10 +3,6 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 instance = ec2.create_instances()
-
-# Create EC2 client
-ec2_client = boto3.client('ec2')
-
 # Retrieve all images
 response = ec2_client.describe_images(Owners=['self'])
 
