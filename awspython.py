4d8cefc0c8371e299496d1c1650ac0e551af3282
amuhtarov <amuhtarov@akumoproject.com>
2023-06-24 15:36:26 -0500

    changes

--- awspython.py
+++ awspython.py
@@ -1,7 +1,7 @@
 import boto3
 
 # Boto3 will automatically use the IAM role credentials assigned to the EC2 instance
-ec2 = boto3.resource('ec2')
+ec2 = boto3.client('ec2')
 
 # Use Boto3 to interact with AWS services
 # For example, list all EC2 instances
@@ -9,4 +9,14 @@ instances = ec2.instances.all()
 for instance in instances:
     print(instance.id)
 imageid = instance.id
-print(imageid)
+
+instance_type = ec2_resource
+
+
+new_instance = ec2.create_instances(
+    ImageId=print(imageid),
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1
+)
+print(new_instance[0].id)
