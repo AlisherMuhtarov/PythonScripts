8a5d76849b570ec05f054df4416d23884404c2f9
amuhtarov <amuhtarov@akumoproject.com>
2023-07-03 17:26:59 -0500

    changes

--- TerraformScripts/lambda_function/lambda_function.py
+++ TerraformScripts/lambda_function/lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
     # Retrieve existing instance information
-    existing_instance_id = 'i-06a773b3654e27f31'  # Replace 'your-existing-instance-id' with the actual existing instance ID
+    existing_instance_id = 'i-0fb81cbe93581432a'  # Replace 'your-existing-instance-id' with the actual existing instance ID
 
     ec2_client = boto3.client('ec2')
     response = ec2_client.describe_instances(InstanceIds=[existing_instance_id])
