1bf936d7b091346dec8131c46f51e03fcf28c87c
amuhtarov <amuhtarov@akumoproject.com>
2023-07-03 17:09:21 -0500

    changes

--- TerraformScripts/main.tf
+++ TerraformScripts/main.tf
@@ -22,22 +22,22 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
  path         = "/"
  description  = "AWS IAM Policy for managing aws lambda role"
- policy = jsonencode(
-    {
+
+ policy = jsonencode({
  "Version": "2012-10-17",
  "Statement": [
    {
+     "Effect": "Allow"
      "Action": [
        "logs:CreateLogGroup",
        "logs:CreateLogStream",
-       "logs:PutLogEvents"
+       "logs:PutLogEvents",
+       "ec2:DescribeInstances"
      ],
      "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
    }
  ]
-}
-)
+})
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
