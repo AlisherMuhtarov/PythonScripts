a76ed2fcd4f6a01ef663f13c3643ad900cbd86b1
amuhtarov <amuhtarov@akumoproject.com>
2023-07-03 11:53:24 -0500

    changes

--- PythonScripts/awspython.py
+++ PythonScripts/awspython.py
@@ -1,26 +1,32 @@
 import boto3
 
-# Retrieve existing instance information
-existing_instance_id = 'i-09fcf0ef0f3c8ffb9'  # Replace 'your-existing-instance-id' with the actual existing instance ID
+def lambda_handler(event, context):
+    # Retrieve existing instance information
+    existing_instance_id = 'i-09fcf0ef0f3c8ffb9'  # Replace 'your-existing-instance-id' with the actual existing instance ID
 
-ec2_client = boto3.client('ec2')
-response = ec2_client.describe_instances(InstanceIds=[existing_instance_id])
-reservations = response['Reservations']
-existing_instance = reservations[0]['Instances'][0]
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_instances(InstanceIds=[existing_instance_id])
+    reservations = response['Reservations']
+    existing_instance = reservations[0]['Instances'][0]
 
-existing_image_id = existing_instance['ImageId']
-existing_instance_type = existing_instance['InstanceType']
+    existing_image_id = existing_instance['ImageId']
+    existing_instance_type = existing_instance['InstanceType']
 
-# Create a new EC2 instance using the retrieved information
-ec2_resource = boto3.resource('ec2')
+    # Create a new EC2 instance using the retrieved information
+    ec2_resource = boto3.resource('ec2')
 
-new_instance = ec2_resource.create_instances(
-    ImageId=existing_image_id,
-    InstanceType=existing_instance_type,
-    MinCount=1,
-    MaxCount=1
-)
+    new_instance = ec2_resource.create_instances(
+        ImageId=existing_image_id,
+        InstanceType=existing_instance_type,
+        MinCount=1,
+        MaxCount=1
+    )
 
-# Print the new instance ID
-new_instance_id = new_instance[0].id
-print("New Instance ID:", new_instance_id)
+    # Print the new instance ID
+    new_instance_id = new_instance[0].id
+    print("New Instance ID:", new_instance_id)
+
+return {
+    'statusCode': 200,
+    'body': 'New Instance Created:' + new_instance_id
+}
--- TerraformScripts/main.tf
+++ TerraformScripts/main.tf
@@ -1,42 +1,60 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "Spacelift_Test_Lambda_Function_Role"
-  assume_role_policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : "sts:AssumeRole",
-          "Principal" : {
-            "Service" : "lambda.amazonaws.com"
-          },
-          "Effect" : "Allow",
-          "Sid" : ""
-        }
-      ]
-    }
-  )
+ name   = "Spacelift_Test_Lambda_Function_Role"
+ assume_role_policy = jsonencode(
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+)
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
-
-  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path        = "/"
-  description = "AWS IAM Policy for managing aws lambda role"
-  policy = jsonencode(
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = jsonencode(
     {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ],
-          "Resource" : "arn:aws:logs:*:*:*",
-          "Effect" : "Allow"
-        }
-      ]
-    }
-  )
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+)
+}
 
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+ type        = "zip"
+ source_dir  = "/home/ec2-user/PythonScripts/"
+ output_path = "/home/ec2-user/PythonScripts/lambda_function.zip"
+}
+resource "aws_lambda_function" "terraform_lambda_func" {
+ filename                       = "home/ec2-user/PythonScripts/lambda_function.zip"
+ function_name                  = "ec2_create_function"
+ role                           = aws_iam_role.lambda_role.arn
+ handler                        = "/home/ec2-user/lambda_function.lambda_handler"
+ runtime                        = "python3.10"
+ depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
