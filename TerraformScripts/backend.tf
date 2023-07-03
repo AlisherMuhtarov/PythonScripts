4ecc1bd5c812edf4d9e174ef8399640e5fdbd7c3
amuhtarov <amuhtarov@akumoproject.com>
2023-07-03 07:24:51 -0500

    terraform files and lambda function added

--- TerraformScripts/backend.tf
+++ TerraformScripts/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-testing-backend-alisher"
-    key    = "session-10/terraform.tfstate"  # Where does terraform need to store your file. Path Or Prefix
-    region = "us-east-1"
-    dynamodb_table = "terraform-testing-state-lock"  # Lock Table
+    bucket         = "terraform-testing-backend-alisher"
+    key            = "session-10/terraform.tfstate" # Where does terraform need to store your file. Path Or Prefix
+    region         = "us-east-1"
+    dynamodb_table = "terraform-testing-state-lock" # Lock Table
   }
 }
