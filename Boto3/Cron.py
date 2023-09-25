030517625ab6840e9c2d3ac11a92bdf7c0c4d270
amuhtarov <amuhtarov@akumoproject.com>
2023-09-25 18:10:23 -0500

    test

--- Boto3/Cron.py
+++ Boto3/Cron.py
@@ -1,6 +1,7 @@
 import boto3
 import datetime
 import pytz
+import time
 
 # Replace with your desired AWS region
 aws_region = 'us-east-1'
@@ -11,6 +12,12 @@ desired_timezone = pytz.timezone('America/Chicago')
 # Compute the launch time in UTC
 desired_launch_time = datetime.datetime.now(desired_timezone)
 desired_launch_time_utc = desired_launch_time.astimezone(pytz.utc)
+time_difference_seconds = (desired_launch_time_utc - datetime.datetime.now(pytz.utc)).total_seconds()
+
+# If the desired launch time is in the future, sleep until then
+if time_difference_seconds > 0:
+    print(f'Sleeping for {time_difference_seconds} seconds until the desired launch time...')
+    time.sleep(time_difference_seconds)
 
 # Initialize the EC2 client
 ec2 = boto3.client('ec2', region_name=aws_region)
